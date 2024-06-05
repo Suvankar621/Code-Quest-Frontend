e3d684428733d9f3e1168941568c8c401c4adc0e
Suvankar Mahato <suvankar_m@pursuitsoftware.com>
2024-06-05 18:24:27 +0530

    Contest Fixed

--- src/components/Admin Panel/CreateContest.jsx
+++ src/components/Admin Panel/CreateContest.jsx
@@ -22,7 +22,7 @@ const CreateContest = () => {
   useEffect(() => {
     const fetchContests = async () => {
       try {
-        const { data } = await axios.get('https://code-quest-backend.onrender.com/api/v1/contest/getcontests', {
+        const { data } = await axios.get('https://code-quest-backend.onrender.com/api/v1/contest/mycontests', {
           withCredentials: true
         });
         setContests(data.contests);
